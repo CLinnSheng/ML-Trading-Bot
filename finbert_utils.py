3c772d0b5ad39190ea408a73e786b628f315fb7c
linnsheng <135851909+CLinnSheng@users.noreply.github.com>
2024-09-17 22:42:35 +0800

    Update finbert_utils.py

--- finbert_utils.py
+++ finbert_utils.py
@@ -2,13 +2,14 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 from typing import Tuple
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
 model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
 labels = ["positive", "negative", "neutral"]
 
 def estimate_sentiment(news):
     if news:
-        tokens = tokenizer(news, return_tensors="pt", padding=True)
+        tokens = tokenizer(news, return_tensors="pt", padding=True).to(device)
         result = model(tokens["input_ids"], attention_mask=tokens["attention_mask"])["logits"]
         result = torch.nn.functional.softmax(torch.sum(result, 0), dim=-1)
         probability = result[torch.argmax(result)]
